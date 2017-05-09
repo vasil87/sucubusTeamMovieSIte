5bf7754680287d83ed8a520694de929e4cfd9de2
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-05-09 22:48:49 +0300

    B

--- test/sethAuth.js
+++
@@ -1,4 +0,0 @@
-export function setAuthHeader() {
-    var token = localStorage.getItem(LOCAL_STORAGE_AUTHKEY_KEY) || sessionStorage.getItem(LOCAL_STORAGE_AUTHKEY_KEY);
-    return { "Authorization": token };
-};
